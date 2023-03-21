c9d8447b7b83b0b80a62d67c6642493d88e0c316
u0 <12602834+tomp736@users.noreply.github.com>
2023-03-21 17:52:43 +0100

    update json

--- src/caseinator.core/Caseinator.cs
+++ src/caseinator.core/Caseinator.cs
@@ -9,25 +9,40 @@ public class Caseinator
 {
     [JsonPropertyName("title")]
     public string Title { get; set; }
-    
-    [JsonPropertyName("description")]
-    public string Description { get; set; } 
 
-    [JsonPropertyName("user_story")]   
+    [JsonPropertyName("background")]
+    public string Background { get; set; }
+
+    [JsonPropertyName("challenges")]
+    public List<string> Challenges { get; set; }
+
+    [JsonPropertyName("solutions")]
+    public List<string> Solutions { get; set; }
+
+    [JsonPropertyName("outcomes")]
+    public List<string> Outcomes { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("user_story")]
     public string UserStory { get; set; }
 
-    [JsonPropertyName("sources_of_complexity")]   
+    [JsonPropertyName("sources_of_complexity")]
     public List<string> SourceOfComplexity { get; set; }
 
-    [JsonPropertyName("refactored_user_stories")]   
+    [JsonPropertyName("refactored_user_stories")]
     public List<string> RefactoredUserStories { get; set; }
 
-    [JsonPropertyName("implementation_issues")]   
+    [JsonPropertyName("implementation_issues")]
     public List<string> ImplementationIssues { get; set; }
 
-    [JsonPropertyName("agile_principles")]   
+    [JsonPropertyName("lean_principles")]
+    public List<string> LeanPrinciples { get; set; }
+
+    [JsonPropertyName("agile_principles")]
     public List<string> AgilePrinciples { get; set; }
-    
+
     [JsonPropertyName("summary")]
-    public string Summary { get; set; } 
+    public string Summary { get; set; }
 }
